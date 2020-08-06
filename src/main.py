ecec12792168ae91a5f96cac3972385bb3e139fd
kotasuzuki <kota.suzuki@ucl.meijo-u.ac.jp>
2020-08-07 00:48:10 +0900

    json追加

--- src/main.py
+++ src/main.py
@@ -2,5 +2,21 @@
 GUI
 '''
 
-import PySimpleGUI as gui
+import PySimpleGUI as sg
 import json
+
+import bt
+
+def loadFromAndroid():
+    '''
+    Bluetoothを用いてAndroidからデータの読み込み
+    :return: json
+    '''
+
+def onScreen(styledata):
+    '''
+    GUI表示する部分
+    :param styledata: Androidから送られてきたJSON形式のデータ
+    :return:
+    '''
+    sg.theme()
